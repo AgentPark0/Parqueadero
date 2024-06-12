cff944f209e2a790184906e17a0a4f96991b92c1
juanes <juanesalar0@gmail.com>
2024-06-12 14:54:43 -0500

    hola

--- parking.py
+++ parking.py
@@ -1,2 +1,9 @@
-print("HOLA BIENVENIDO AL PARQUEADERO")
-print(float("Que tipo de vehiculo desea registrar?"))
+def informacion():
+    print("HOLA BIENVENIDO AL PARQUEADERO")
+    print("1. Automoviles (3000$ / min)")
+    print("2. Camioneta (2000$ / min)")
+    print("3. Motocicleta (1000 / min)")
+    print(float("Que tipo de vehiculo desea registrar?"))
+
+    
+
